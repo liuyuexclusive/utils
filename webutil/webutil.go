69bdfe9c39d98cbea6cd1a6dda1d298dea22f4ec
liuyu <453017973@qq.com>
2019-10-29 11:47:29 +0800

    uodate AllowOrigin

--- webutil/webutil.go
+++ webutil/webutil.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"strings"
 	"time"
 
@@ -36,9 +37,14 @@ func UseSwagger(path string, url string, router *gin.Engine) {
 func AllowOrigin() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "Content-Type")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, AccessToken,X-CSRF-Token, Authorization, Token")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		c.Header("Access-Control-Max-Age", "86400") // 缓存请求信息 单位为秒
 		// c.Header("Access-Control-Allow-Credentials", "false")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		c.Next()
 	}
 }
 
