7fbcb03ffe2680de9dfd31eabe0dca52ce218c71
liuyu <453017973@qq.com>
2020-04-27 22:11:19 +0800

    fix

--- webutil/webutil.go
+++ webutil/webutil.go
@@ -203,14 +203,20 @@ func Startup(name string, starter Starter, opts ...Option) error {
 	}
 
 	var swaggerPath, swaggerURL string
-	if options.Port != "" {
-		swaggerPath = "/swagger/*any"
-		swaggerURL = fmt.Sprintf("http://%s%s/swagger/doc.json", config.HostIP, options.Port)
-	} else {
-		head := strings.TrimPrefix(name, "go.micro.api.")
-		swaggerPath = fmt.Sprintf("/%s/swagger/*any", head)
-		swaggerURL = fmt.Sprintf("http://%s:%s/%s/swagger/doc.json", config.HostIP, config.APIPort, head)
-	}
+
+	head := strings.TrimPrefix(name, "go.micro.api.")
+	swaggerPath = fmt.Sprintf("/%s/swagger/*any", head)
+	swaggerURL = fmt.Sprintf("http://%s:%s/%s/swagger/doc.json", config.HostIP, config.APIPort, head)
+
+	// var swaggerPath, swaggerURL string
+	// if options.Port != "" {
+	// 	swaggerPath = "/swagger/*any"
+	// 	swaggerURL = fmt.Sprintf("http://%s%s/swagger/doc.json", config.HostIP, options.Port)
+	// } else {
+	// 	head := strings.TrimPrefix(name, "go.micro.api.")
+	// 	swaggerPath = fmt.Sprintf("/%s/swagger/*any", head)
+	// 	swaggerURL = fmt.Sprintf("http://%s:%s/%s/swagger/doc.json", config.HostIP, config.APIPort, head)
+	// }
 
 	UseSwagger(swaggerPath, swaggerURL, router)
 
