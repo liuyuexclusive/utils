dd7a21e1a994ae7807f3cc0078c8ac900e5db715
liuyu <453017973@qq.com>
2020-06-13 11:22:49 +0800

    test

--- redis/redis.go
+++ redis/redis.go
@@ -7,15 +7,20 @@ import (
 	r "github.com/go-redis/redis"
 )
 
+type Client struct {
+	*r.Client
+}
+
 // Open 打开redis
-func Open(f func(client *r.Client) error) error {
+func Open(f func(client *Client) error) error {
 	client := r.NewClient(&r.Options{
 		Addr:     appconfig.MustGet().RedisAddress,
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 	defer client.Close()
-	if err := f(client); err != nil {
+	newClient := &Client{Client: client}
+	if err := f(newClient); err != nil {
 		logrus.Error(err)
 		return err
 	}
